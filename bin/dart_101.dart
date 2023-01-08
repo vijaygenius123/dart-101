b391deb1ec0a1fbed96d509bd14a0b21486d6e0d
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2023-01-08 22:46:18 +0530

    Variables

--- bin/dart_101.dart
+++ bin/dart_101.dart
@@ -1,3 +1,17 @@
 void main(List<String> arguments) {
   print('Hello world!');
+
+  int num1 = 10;
+  print(num1);
+
+  double num2 = 10.5;
+  print(num1 + num2);
+
+  print((num1 + num2).runtimeType);
+
+  String name = 'John';
+  print(name);
+
+  final fullName = 'John Doe';
+  print(fullName);
 }
